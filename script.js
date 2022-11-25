a14a9189e8b50118ecb21df8c5b52c14ecfab76d
ArryPutra <113241887+ArryPutra@users.noreply.github.com>
2022-11-25 23:44:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-    <script>
-      AOS.init();
-
 let body = document.body;
-
         function menu() {
             body.classList.toggle("script");
         }
